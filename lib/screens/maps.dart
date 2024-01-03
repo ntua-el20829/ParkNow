3aadfd7a902f8ced44540fc4289fcf014ff47b1a
ntua-el20829 <stefanos.giannakopoulosece@gmail.com>
2024-01-04 00:44:35 +0200

    navbar created at maps.dart

--- lib/screens/maps.dart
+++ lib/screens/maps.dart
@@ -12,8 +12,9 @@ class Maps extends StatefulWidget {
 
 class _MapsState extends State<Maps> {
   int? userId;
-  final storage =
-      new FlutterSecureStorage(); // Ensure you have an instance of FlutterSecureStorage
+
+  final storage = FlutterSecureStorage();
+  int _selectedIndex = 2;
 
   @override
   void initState() {
@@ -30,11 +31,29 @@ class _MapsState extends State<Maps> {
         userId = int.tryParse(decodedToken['sub']
             .toString()); // Extract the user ID and ensure it's an integer
       });
-      print(userId);
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('User has not granted access or timed out')));
       Navigator.of(context).pushReplacementNamed('/');
+    }
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    // Implement navigation logic depending on the index
+    // For example:
+    if (index == 0) {
+      Navigator.of(context).pushReplacementNamed('/profile');
+    } else if (index == 1) {
+      Navigator.of(context).pushReplacementNamed('/favourites');
+    } else if (index == 2) {
+      // do nothing
+    } else if (index == 3) {
+      Navigator.of(context).pushReplacementNamed('/parked_cars');
+    } else if (index == 4) {
+      Navigator.of(context).pushReplacementNamed('/more');
     }
   }
 
@@ -71,6 +90,34 @@ class _MapsState extends State<Maps> {
                 ],
               )
             : CircularProgressIndicator(), // Show a loading indicator while waiting
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_outlined),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_car_outlined),
+            label: 'Parked Cars',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more_horiz),
+            label: 'More',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
